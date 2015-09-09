632d878972e5980f4f85116acdd4e1dca44bf5b6
Zachary Dixon <rissk13@gmail.com>
2015-09-09 17:43:15 -0500

    Fixed conversations call

--- client/scripts/routes.import.jsx
+++ client/scripts/routes.import.jsx
@@ -50,6 +50,8 @@ managedAccounts.route('/:business_id/tweets', {
   action: (params, queryParams) => {
     // Needed for refreshing page or navigating directly to this route
     Session.set('business_id', parseInt(params.business_id));
+    // Reset loadMoreConvos to call initially
+    Session.set('loadMoreConvos', true)
     ReactLayout.render(LayoutManager, {
       layout: MainLayout,
       content: <TweetsPage />
